fb1ffacd96c4a0cbccbc8b3a5b897861f6c57c2a
b13d <4.topppp@mail.ru>
2023-07-06 02:43:40 +0400

    change direction bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const variants = {};
 
   return (
-    <main className="flex bg-gradient-to-b from-[#0d1117] to-[#232e3f] min-h-screen flex-col items-center justify-between">
+    <main className="flex bg-gradient-to-b from-[#232e3f] to-[#0d1117] min-h-screen flex-col items-center justify-between">
 
 
       <Main />
